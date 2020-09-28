d00f17218495b88c9e5b36ed4f15fe03fd6c0fa1
yashbonde <bonde.yash97@gmail.com>
2020-09-29 00:29:22 +0530

    complete downloader

--- prep_data.py
+++ prep_data.py
@@ -14,7 +14,6 @@ from datetime import datetime
 from argparse import ArgumentParser
 
 from datetime import timedelta, date
-
 def daterange(start_date, end_date):
     for n in range(int((end_date - start_date).days)):
         yield start_date + timedelta(n)
@@ -44,6 +43,7 @@ COLUMNS = [
 ]
 COL_NAMES = [x[0] for x in COLUMNS]
 UNITS = [x[1] for x in COLUMNS]
+BAD_ROW_SUM = -169983.0
 
 
 def open_csv(f):
@@ -70,25 +70,7 @@ def open_csv(f):
         )
     return df, p
 
-
-def download_file(url, folder):
-    local_filename = folder + "/" +url.split('/')[-1]
-    # NOTE the stream=True parameter below
-    with requests.get(url, stream=True) as r:
-        r.raise_for_status()
-        with open(local_filename, 'wb') as f:
-            for chunk in tqdm(r.iter_content(chunk_size=8192)):
-                # If you have chunk encoded response uncomment if
-                # and set chunk_size parameter to None.
-                #if chunk:
-                f.write(chunk)
-    return local_filename
-
-
-
 if __name__ == "__main__":
-    BAD_ROW_SUM = -169983.0
-
     args = ArgumentParser()
     args.add_argument('-d', action='store_true', help = "download all files")
     args.add_argument('-x', action='store_true', help="extract zip files")
@@ -96,10 +78,9 @@ if __name__ == "__main__":
     args.add_argument('--folder', type = str, default="INMET", help = "folder to store all the years data")
     args = args.parse_args()
 
-    urls = [f"https://portal.inmet.gov.br/uploads/dadoshistoricos/{x}.zip" for x in range(2000, 2021)]
-    # print(urls)
     os.makedirs(args.folder, exist_ok=True)
 
+    urls = [f"https://portal.inmet.gov.br/uploads/dadoshistoricos/{x}.zip" for x in range(2000, 2021)]
     if args.d:
         for u in urls:
             print("fetching url:", u)
@@ -123,33 +104,10 @@ if __name__ == "__main__":
             start_date = date(*list(map(int, start_date.split("-")))[::-1])
             end_date = date(*list(map(int, end_date.split("-")))[::-1])
 
-            # df, meta = open_csv(f)
-
-            # df = df.dropna(axis=1)
-            # for col in df.columns[2:]:
-            #     v = getattr(df, col).values
-            #     v = [float(str(x).replace(",", ".")) for x in v]
-            #     setattr(df, col, v)
-
-            # brs = BAD_ROW_SUM; idx = 0
-            # while brs == BAD_ROW_SUM:
-            #     brs = sum(df.loc[idx][2:])
-            #     idx += 1
-
-            # df.columns = COL_NAMES
-
             for single_date in daterange(start_date, end_date):
                 d = single_date.strftime("%Y-%m-%d")
                 consol.setdefault(d, [])
                 consol[d].append(wsid)
-
-            
-
-            # for li in range(idx, df.shape[0]):
-            #     dt = f"{df.loc[idx].date}_{df.loc[idx].date}"
-
-            #     consol.setdefault(dt, [])
-            #     consol[dt].append(wsid)
 
         for k,v in consol.items():
             consol[k] = ",".join(v)
@@ -163,6 +121,3 @@ if __name__ == "__main__":
 
     print("completed, exiting")
 
-
-
-    
